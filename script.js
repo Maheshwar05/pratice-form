395f4065563cc9e786f05262d4c6d23b6013b26d
Maheshwar05 <117148809+Maheshwar05@users.noreply.github.com>
2023-03-21 13:31:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+const form = document.querySelector('#form');
+const usernameInput = document.querySelector('#username');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#password2');
+
+form.addEventListener('submit', (event)=>{
+    
+    validateForm();
+    console.log(isFormValid());
+    if(isFormValid()==true){
+        form.submit();
+     }else {
+         event.preventDefault();
+     }
+
+});
+function isFormValid(){
+    const inputContainers = form.querySelectorAll('.form-control');
+    let result = true;
+    inputContainers.forEach((container)=>{
+        if(container.classList.contains('error')){
+            result = false;
+        }
+    });
+    return result;
+}
+
+function validateForm() {
+	const usernameValue = username.value.trim();
+    const lastnameValue = lastname.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+
+    if(usernameInput.value ==''){
+        setError(usernameInput, 'Name can not be empty');
+    }else if(usernameInput.value.length <5 || usernameInput.value.trim().length > 15){
+        setError(usernameInput, 'Name must be min 5 and max 15 charecters');
+    }else {
+        setSuccess(usernameInput);
+    }
+    if(emailInput.value ==''){
+        setError(emailInput, 'Provide email address');
+    }else if(isEmailValid(emailInput.value)){
+        setSuccess(emailInput);
+    }else{
+        setError(emailInput, 'Provide valid email address');
+    }
+    if(passwordInput.value.trim()==''){
+        setError(passwordInput, 'Password can not be empty');
+    }else if(passwordInput.value.trim().length <6 || passwordInput.value.trim().length >20){
+        setError(passwordInput, 'Password min 6 max 20 charecters');
+    }else {
+        setSuccess(passwordInput);
+    }
+    if(confirmPasswordInput.value.trim()==''){
+        setError(confirmPasswordInput, 'Password can not be empty');
+    }else if(confirmPasswordInput.value !== passwordInput.value){
+        setError(confirmPasswordInput, 'Password does not match');
+    }else {
+        setSuccess(confirmPasswordInput);
+    }
+}
+
+function setError(element, errorMessage) {
+    const parent = element.parentElement;
+    if(parent.classList.contains('success')){
+        parent.classList.remove('success');
+    }
+    parent.classList.add('error');
+    const paragraph = parent.querySelector('small');
+    paragraph.textContent = errorMessage;
+}
+
+function setSuccess(element){
+    const parent = element.parentElement;
+    if(parent.classList.contains('error')){
+        parent.classList.remove('error');
+    }
+    parent.classList.add('success');
+}
+
+function isEmailValid(email){
+    return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    
+}
